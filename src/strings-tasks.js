00c66eb236a270582fd16b97a1e86d26f5cab546
Ivan <vaniamar@mail.ru>
2025-09-19 20:51:26 +0700

    test

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -37,8 +37,8 @@ function getStringLength(/* value */) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  return Boolean(typeof value === 'string') || value instanceof String;
 }
 
 /**
